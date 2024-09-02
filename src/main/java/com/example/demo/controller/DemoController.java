5d34156628d2910a97aebd8e478eb0e1583b5dde
praveena <praveena@gmail.com>
2024-09-02 11:01:41 +0530

    first commit

---
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+@RequestMapping("/demo/api")
+public class DemoController {
+	
+	@GetMapping("/welcome")
+	public String getMethodName() {
+		return "welcome to demo application";
+	}
+	
+	
+
+}
